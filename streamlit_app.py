92a19bbd0e9b0cafbd390712b762f544d64d1302
syedalin1 <saimindset786@gmail.com>
2026-09-02 11:09:56 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,17 @@
 import streamlit as st
 import pandas as pd
 
-st.title("TikTok Analytics App")
+st.title("Amelle Analytics App")
 
-st.write("Upload your TikTok data and I will analyse it.")
+st.write("Upload your TikTok Excel file and I will analyse it.")
 
-uploaded_file = st.file_uploader("Upload your TikTok CSV file", type=["csv"])
+uploaded_file = st.file_uploader(
+    "Upload your TikTok Excel file",
+    type=["xlsx"]
+)
 
 if uploaded_file is not None:
-    data = pd.read_csv(uploaded_file)
+    data = pd.read_excel(uploaded_file)
 
     st.subheader("Your TikTok Data")
     st.dataframe(data)
