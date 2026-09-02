b498adb73b79cf55c71798fee49b7eb9acad4112
syedalin1 <saimindset786@gmail.com>
2026-09-02 12:14:40 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,16 +27,14 @@ if uploaded_file is not None:
     st.write(list(data.columns))
     st.subheader("Top 10 TikTok Videos")
 
-    view_columns = [col for col in data.columns if "view" in str(col).lower()]
+        st.subheader("Top 10 TikTok Videos")
 
-    if view_columns:
-        view_column = "playCount"
-
+    if "playCount" in data.columns:
         top_10 = data.sort_values(
-            by=view_column,
+            by="playCount",
             ascending=False
         ).head(10)
 
         st.dataframe(top_10, use_container_width=True)
     else:
-        st.warning("I could not automatically find the Views column.")
+        st.warning("The playCount column was not found in the Excel file.")
